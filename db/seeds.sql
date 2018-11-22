bbeebaf7a9dfd22918c12f499b89a1aa1831dec7
Akanksha Kapoor <agaur05@gmail.com>
2018-11-21 19:16:59 -0600

    adding all burger files

---
+++ db/seeds.sql
@@ -0,0 +1,5 @@
+INSERT INTO BURGERS(burger_name,devoured)values("chicken Tikka",false);
+INSERT INTO BURGERS(burger_name,devoured)values("Paneer Tikka",true);
+INSERT INTO BURGERS(burger_name,devoured)values("Chile Mushrooms",false);
+INSERT INTO BURGERS(burger_name,devoured)values("Cheese Spinach",false);
+INSERT INTO BURGERS(burger_name,devoured)values("Jalapeno Mushrooms",true);
