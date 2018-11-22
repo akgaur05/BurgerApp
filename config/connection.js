2f35c2b1ee1d50a77c04bcbb701b620310fb0192
Akanksha Kapoor <agaur05@gmail.com>
2018-11-21 19:55:34 -0600

    connection.js changes

--- config/connection.js
+++ config/connection.js
@@ -7,12 +7,13 @@ if (process.env.JAWSDB_URL) {
 } else {
     connection = mysql.createConnection({
         host: 'localhost',
+        port: 8889,
         user: 'root',
         password: 'root',
         database: 'burgers_db'
     });
 
-
+}
 connection.connect(function (err) {
     if (err) {
         console.error("error connecting: " + err.stack);
